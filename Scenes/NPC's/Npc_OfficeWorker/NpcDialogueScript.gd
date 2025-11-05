0431bf3352d0c39efab78196317d48d14bdabbaa
Filipe Almeida <f.almeida87@gmail.com>
2025-11-05 19:26:48 -0300

    initial commit

---
+++ Scenes/NPC's/Npc_OfficeWorker/NpcDialogueScript.gd
@@ -0,0 +1,58 @@
+extends Area2D
+
+#TextBubble
+var talking = false
+var player_entered = false
+var timeline1 = false
+var timeline2 = false
+@export var speech : Label
+@export var line1 = []
+
+@export var show_text_duration : float = 1.0
+@export var silence_duration : float = 1.0
+
+#NOTE Displays the text above an NPC and removes it after a slight delay
+
+func _ready():
+	speech.text = ""
+	print("connect")
+
+func _input(event: InputEvent) -> void:
+	if player_entered and event and Input.is_action_pressed("Enter"):
+		# Only start a new dialog if no dialog is currently active
+		if Dialogic.current_timeline == null:
+			if not timeline1:
+				Dialogic.start("res://Dialogs/timeline1.dtl")
+				timeline1 = true
+			elif not timeline2:
+				Dialogic.start("res://Dialogs/timeline2.dtl")
+				timeline2 = true
+
+#region Signal Entering & Exiting
+
+func _on_body_entered(body):
+	player_entered = true
+	if body.is_in_group("Player") and not talking:
+		#talk_tween()
+		speech.text = line1[randi() % line1.size()]
+
+func _on_body_exited(body):
+	player_entered = false
+	if body.is_in_group("Player") and talking:
+		await get_tree().create_timer(show_text_duration).timeout #Show the text for a short while after the player leaves
+		speech.text = "*murmur*"
+		await get_tree().create_timer(silence_duration).timeout #Show "murmur" for a short while before going silent
+		speech.text = ""
+		talking = false
+
+
+#endregion
+
+#Animate the NPC talking with some simple tweening
+func talk_tween():
+	talking = true
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.2)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+	tween.tween_property(self, "scale", Vector2.ONE, 0.2)
---
+++ Scenes/Player/Scripts/States/PlayerIdleState.gd
@@ -0,0 +1,15 @@
+extends State
+class_name PlayerIdle
+
+@export var animator : AnimationPlayer
+
+func Enter():
+	animator.play("Idle")
+	var player = get_tree().get_first_node_in_group("Player")
+	
+func Update(_delta : float):
+	if(Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown").normalized()):
+		state_transition.emit(self, "Moving")
+		
+	if Input.is_action_just_pressed("Punch")  or Input.is_action_just_pressed("Kick"):
+		state_transition.emit(self, "Attacking")
