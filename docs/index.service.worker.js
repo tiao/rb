177c476522217abdd79f6e2d353f9cff88f53baf
Filipe Almeida <f.almeida87@gmail.com>
2025-11-07 18:13:10 -0300

    update docs

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762395925|11507386890';
+const CACHE_VERSION = '1762397629|13211886035';
 /** @type {string} */
 const CACHE_PREFIX = 'RB-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
